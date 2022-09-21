52be323584af0035f945dc0f81150085cddba23d
ERLMKea <erlm@kea.dk>
2022-09-21 09:51:25 +0200

    countries endpoint

--- src/main/java/com/example/presidentcountryi/controller/PresidentRestController.java
+++ src/main/java/com/example/presidentcountryi/controller/PresidentRestController.java
@@ -1,6 +1,8 @@
 package com.example.presidentcountryi.controller;
 
+import com.example.presidentcountryi.model.Country;
 import com.example.presidentcountryi.model.President;
+import com.example.presidentcountryi.repository.CountryRepository;
 import com.example.presidentcountryi.repository.PresidentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,14 @@ public class PresidentRestController {
 
     @Autowired
     PresidentRepository presidentRepository;
+
+    @Autowired
+    CountryRepository countryRepository;
+
+    @GetMapping("/countries")
+    public List<Country> getCountries() {
+        return countryRepository.findAll();
+    }
 
     @GetMapping("/presidents")
     public List<President> getPresidents() {
--- src/main/java/com/example/presidentcountryi/model/President.java
+++ src/main/java/com/example/presidentcountryi/model/President.java
@@ -1,5 +1,7 @@
 package com.example.presidentcountryi.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import javax.persistence.*;
 
 @Entity
